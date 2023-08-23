7c00326eb8fd456a28297ef6171740bb35b37d9f
Dmitry Samarin <d.samarin@solarlab.ru>
2023-08-23 17:44:27 +0300

    initial commit

---
+++ src/Dashboard/Hosts/Dashboard.Hosts.Api/Controllers/PostController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Hosts.Api.Controllers;
+
+/// <summary>
+/// Контроллер для работы с объявлениями.
+/// </summary>
+public class PostController : ControllerBase
+{
+    
+}
